714b6318bdde3f85503528e62b7ca1710eb5ef5b
leraandreeva97 <leraandreeva97@gmail.com>
2017-06-23 21:38:05 +0300

    lab3

---
+++ Assets/Mushroom.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : Collectable {
+	protected override void OnRabitHit (HeroRabit rabit)
+	{
+		//Level.current.addCoins (1);
+		rabit.ScaleUp();
+		this.CollectedHide ();
+			
+	}
+}
